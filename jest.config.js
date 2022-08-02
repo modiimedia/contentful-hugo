1fd5ade2a67853328bb95a2ea5fd2a30608493cb
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2022-08-02 09:04:05 -0500

    update dependencies - upgrade contentful to v9.1.33
    update devDependencies
    - @types/jest to 28.1.6
    - @typescript-eslint/eslint-plugin to 5.32.0
    - @typescript-eslint/parser to 5.32.0
    - eslint to 8.21.0
    - jest to 28.1.3
    - ts-jest to 28.0.7

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules', '/dist/'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
         '^@helpers/(.*)$': '<rootDir>/src/helpers/$1',
